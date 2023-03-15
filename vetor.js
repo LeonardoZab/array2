54278fb05b96712f3b0d8bc4ac6363b88b98c1c5
Leonardo <leonardo_l_zabel@estudante.sesisenai.org.br>
2023-03-15 09:36:00 -0300

    arrays expandidos

--- vetor.js
+++ vetor.js
@@ -1,13 +1,15 @@
 let valor_cidade = []
+let res = document.getElementById('res')
 
 function inserir(){
     let cidade = document.getElementById("cidade").value
     valor_cidade.push(cidade)
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade + ", "
 }
 
 function deletar(){
-    let cidade = document.getElementById("cidade").value
     valor_cidade.pop(cidade)
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade
 }
