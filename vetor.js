538fa1d785ea14fa073947dfd87afe19b5e38b93
Leonardo <leonardo_l_zabel@estudante.sesisenai.org.br>
2023-03-15 10:26:10 -0300

    botões de excluir e incluir no inicio adicionados

--- vetor.js
+++ vetor.js
@@ -8,8 +8,24 @@ function inserir(){
     res.innerHTML = valor_cidade + ", "
 }
 
+function inserir_inicio(){
+    let cidade = document.getElementById("cidade").value
+    valor_cidade.unshift(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+
+
+}
+
 function deletar(){
     valor_cidade.pop(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
+
+
+function excluir_inicio(){
+    valor_cidade.shift(cidade)
     console.log(valor_cidade)
     res.innerHTML = valor_cidade
 }
